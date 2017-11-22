b521eb5fdafe30ef6969e7a814749a1499263e6f
CyanCoco <concyan@outlook.com>
2017-11-22 11:47:05 +0800

    完善reset样式，修改src/index.js文件

--- src/index.js
+++ src/index.js
@@ -2,27 +2,15 @@
  * 该文件请勿动
  */
 import React, {Component} from 'react';
-
 import ReactDOM from 'react-dom';
-
 import {Provider} from 'react-redux';
-
 import Store from './store/index';
-
 import App from './containers/App/index';
-import Header from './components/Header/home_header';
-import Lunbotu from './components/Lunbotu/Lunbotu';
-import Content from './containers/Home/index'
-import './common/style/reset.less';
-
-let images=[
-    require('./components/Lunbotu/2.jpg'),
-    require('./components/Lunbotu/1.jpg'),
-    require('./components/Lunbotu/2.jpg')
-];
-{/*<Lunbotu images={images}/>,*/}
+import './common/style/reset.min.less';
 
 ReactDOM.render(
-    <Content/>,
+    <Provider store={Store}>
+        <App/>
+    </Provider>,
     document.getElementById('app')
 );
