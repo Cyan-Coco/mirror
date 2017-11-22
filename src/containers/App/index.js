0bac62aa7083fb1d551a8a93482b11e900642b7e
tiantian <1390693931@qq.com>
2017-11-22 16:04:59 +0800

    提交course模块

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import {createHashHistory} from 'history';
 import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import {ConnectedRouter} from 'react-router-redux'
-import DefaultRoute from "../DefaultRoute/DefaultRoute";
+import Tab from '../../components/Tab/index'
 const history = createHashHistory();
 /******************************************************/
 import Home from '../Home/index';
@@ -11,17 +11,20 @@ import Course from '../Course/index';
 import Study from '../Study/index';
 import Profile from '../Profile/index';
 import './index.less';
+import Detail from "../Course/Detail/index";
 /******************************************************/
 class App extends Component {
     render() {
         return (
             <ConnectedRouter history={history}>
                 <div className="App">
-                    <Route path='/' exact component={DefaultRoute}/>
-                    <Route path='/home' component={Home}/>
+                    <Route path='/' exact component={Home}/>
                     <Route path='/course' component={Course}/>
                     <Route path='/study' component={Study}/>
                     <Route path='/profile' component={Profile}/>
+                    <Route path='/detail/:id' component={Detail}/>
+
+                    <Tab/>
                 </div>
             </ConnectedRouter>
         )
