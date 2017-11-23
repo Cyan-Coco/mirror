66623a6299a7bc1dff5acdde17f12e7fabada363
zhaopei <752948208@qq.com>
2017-11-23 19:57:17 +0800

    修改首页reducers

--- src/store/reducers/home.js
+++ src/store/reducers/home.js
@@ -3,22 +3,24 @@
  */
 import * as types from '../action-types'
 let initState = {
-  article:{}
+  sliders:[],
+  list: []
 }
 
-export default function (state=initState, action) {
+export default function (state = initState, action) {
   switch (action.type) {
     //获取首页中官方推荐的数据
     case types.FETCH_OFFICE:
       return {
         ...state,
-        article:action.payload.articles
-        }
+        list: action.payload.articles.list
+      }
     //获取首页中开发者头条的数据
     case types.FETCH_DEVHOTS:
-      return{
+      return {
         ...state,
-        article:action.payload
+        sliders:action.payload.slider.list,
+        list: action.payload.articles.list
       }
     default:
       return state
